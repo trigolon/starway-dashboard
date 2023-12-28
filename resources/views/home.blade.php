544aa0c62ae5f97522e88bf959a5848a4b6ff091
ale <alejandro.liu@davinci.edu.ar>
2023-12-28 10:33:51 -0300

    test commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+                <p>test</p>
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
